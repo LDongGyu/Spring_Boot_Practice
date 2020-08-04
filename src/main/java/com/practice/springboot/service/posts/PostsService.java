c7a71570c845497cb5891da6663c8786f78c171f
LDongGyu <ehdrbchd@naver.com>
2020-08-04 23:27:26 +0900

    feat: post save api 구현 및 테스트 코드 작성

---
+++ src/main/java/com/practice/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.practice.springboot.service.posts;
+
+import com.practice.springboot.domain.posts.PostsRepository;
+import com.practice.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
